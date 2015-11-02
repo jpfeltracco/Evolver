af46963b0e09aaf1bf19429cbb3820fd373b1634
jpfeltracco <jpfeltracco@gatech.edu>
2015-11-02 23:21:42 +0100

    Moved EA to GUI

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -1,13 +1,5 @@
 package core;
 
-import org.neuroph.util.TransferFunctionType;
-
-import controllers.Controller;
-import controllers.MLP;
-import evolver.EvolutionAlgorithm;
-import evolver.EvolutionAlgorithm.Type;
-import simulations.Simulation;
-import simulations.XOR;
 import ui.GUI;
 
 public class Main {
@@ -15,20 +7,6 @@ public class Main {
 	public static boolean runThreads = true;
 	
 	public static void main(String[] args) {
-        Simulation s = new XOR();
-        Controller c = new MLP(s.getNumInputs(), s.getNumOutputs(), TransferFunctionType.SIN, 4, 4);
-        EvolutionAlgorithm ea = new EvolutionAlgorithm(s, c);
-        s.setEvolutionAlgorithm(ea);
-        ea.setReproductionType(Type.RANDOM);
-        ea.setGenerationMultiplier(10);
-        ea.setMutationAmt(0.13f);
-        ea.setMutationRate(0.15f);
-        ea.setFoundersPercent(0.5f);
-        ea.setGamesPerElement(5);
-
-        System.out.println("Starting Simulation: " + s);
-        new Thread(ea).start();
-
         GUI.run();
 	}
 }
--- src/main/java/ui/GUI.java
+++ src/main/java/ui/GUI.java
@@ -1,15 +1,25 @@
 package ui;
 
+import org.neuroph.util.TransferFunctionType;
+
+import controllers.Controller;
+import controllers.MLP;
+import evolver.EvolutionAlgorithm;
+import evolver.EvolutionAlgorithm.Type;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import simulations.Simulation;
+import simulations.XOR;
 
 /**
  * Created by jpfel on 11/2/2015.
  */
 public class GUI extends Application {
+	
+	Thread eaThread;
 
     public static void run() {
         launch();
@@ -24,5 +34,25 @@ public class GUI extends Application {
         primaryStage.setTitle("Genetic Algorithm Framework");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+    
+    private void startGA() {
+    	Simulation s = new XOR();
+        Controller c = new MLP(s.getNumInputs(), s.getNumOutputs(), TransferFunctionType.SIN, 4, 4);
+        EvolutionAlgorithm ea = new EvolutionAlgorithm(s, c);
+        s.setEvolutionAlgorithm(ea);
+        ea.setReproductionType(Type.RANDOM);
+        ea.setGenerationMultiplier(10);
+        ea.setMutationAmt(0.13f);
+        ea.setMutationRate(0.15f);
+        ea.setFoundersPercent(0.5f);
+        ea.setGamesPerElement(5);
+        eaThread = new Thread(ea);
+        eaThread.start();
+    }
+    
+    @Override
+    public void stop() {
+    	
     }
 }
