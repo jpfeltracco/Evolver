e69617b505f08e94eba8d93a76a9d546d4ba4d74
Jeremy Feltracco <jpfeltracco@gatech.edu>
2016-04-15 15:59:32 -0400

    Got rid of useless Random

--- src/core/Main.java
+++ src/core/Main.java
@@ -1,13 +1,5 @@
 package core;
 
 import ui.controllers.GUI;
-import java.util.Random;
 
-public class Main {
-    public static Random rand = new Random();
-
-    public static void main(String[] args) {
-        System.out.println("Working Directory = " + System.getProperty("user.dir"));
-        GUI.run();
-    }
-}
+public class Main { public static void main(String[] args) { GUI.run(); } }
